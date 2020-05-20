eb1f8b231ed39c94aa6b6655af05191c4ee0e321
kimani <kimanihube@gmail.com>
2020-05-20 17:50:16 +0300

    removed sequelize models

--- controllers/sequelize.js
+++ controllers/sequelize.js
@@ -3,7 +3,7 @@ const logger = require('./logger');
 const user = 'root';
 const password = process.env.MYSQL_ROOT_PASSWORD;
 
-module.exports = new Sequelize('digitalmatatus', user, password, {
+module.exports = new Sequelize('digital_matatus', user, password, {
     host: '127.0.0.1',
     dialect: 'mariadb'
 });;
